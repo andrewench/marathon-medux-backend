ada9ecde9704b86faca3032a4e24493f14f3bbe2
Andrew <andrewench.react.dev@gmail.com>
2023-11-07 16:48:18 +0300

    add prisma base configuration
    - Add additionally deps.

--- app/modules/auth/auth.module.ts
+++ app/modules/auth/auth.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common'
 import { JwtModule } from '@nestjs/jwt'
 
+import { PrismaService } from '@/services'
+
 import { AuthController } from './auth.controller'
 
 import { AuthService } from './auth.service'
@@ -8,6 +10,6 @@ import { AuthService } from './auth.service'
 @Module({
   imports: [JwtModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, PrismaService],
 })
 export class AuthModule {}
